eb6c8b5eeb5cf1387b30f0f1eab9d7286b0c003c
EricKogut <eric.jkogut@gmail.com>
2020-02-09 12:47:17 -0500

    Febuary 9th - Basic Player Setup
    Fixed:
    
    player
    - fixed player movement
    	- movement was assigned to animation by accident
    	- direction only updates if the player moves! otherwise it would overwrite to (0,0)
    - fixed player sprite pivot point
    
    Added:
    
    Scripts/objecsts
    - generic ability
    - shooting ability
    - various projectile sprites
    - created prefab for bullet
    - bulletmovement script
    
    Changed:
    
    - player movement now has a slot for ability and added a variable for direction
    
    Other minor stuff:
    
    - created new abilties and game object folder
    - added a default animation (still doesnt work :/)
    - zoomed the camera out a little bit to make the map smaller
    -

---
+++ Eng Simulator/Assets/Scenes/Scripts/Abilities/GenericAbility.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//References for objects
+
+[CreateAssetMenu(menuName ="Scripts/Abilities/GenericAbility", fileName = "New Generic Ability")]
+
+
+
+public class GenericAbility : ScriptableObject
+{
+    public virtual void Ability(Vector2 playerPosition, Vector2 currentDirection, Animator playerAnimator, 
+    Rigidbody2D playerRB = null)
+    {
+
+
+    } 
+
+}
+
