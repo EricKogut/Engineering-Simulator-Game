27c888d50879c269026436dd6d8e91dd4be26802
EricKogut <eric.jkogut@gmail.com>
2020-02-09 22:29:44 -0500

    Added scene switcher

---
+++ Eng Simulator/Assets/Scenes/Scripts/SceneSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public string myScene;
+
+    public void collision(Collider2D theCollider)
+    {   
+
+        if(theCollider.CompareTag("Player") && !theCollider.isTrigger){
+
+            SceneManager.LoadScene(myScene);
+        }
+
+
+
+    }
+
+
+
+
+}
