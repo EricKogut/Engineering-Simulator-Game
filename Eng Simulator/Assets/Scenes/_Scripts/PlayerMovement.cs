92ba135390f0f237d5b639aa90d0ccef11f2e6ef
EricKogut <eric.jkogut@gmail.com>
2020-03-14 19:07:08 -0400

    Added Ultimate + Dash Ability

--- Eng Simulator/Assets/Scenes/_Scripts/PlayerMovement.cs
+++ Eng Simulator/Assets/Scenes/_Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public float playerSpeed;
 public GenericAbility myAbility;
 private Vector2 currentDirection = new Vector2(1f,0f);
 
-
+private BoxCollider2D playerBoxCollider;
 private Rigidbody2D playerRigidBody;
 private Vector3 movement;
 public static float energy = 100f;
@@ -27,6 +27,7 @@ private Animator playerAnimation;
         
         playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
+        playerBoxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -36,10 +37,12 @@ private Animator playerAnimation;
         //This will allow players to use abilities/shoot etc
 
         if(Input.GetButtonDown("Ability")){
-            Debug.Log("This is a test");
-
             myAbility.Ability(transform.position, currentDirection, playerAnimation, playerRigidBody );
+        }
 
+        if(Input.GetButtonDown("UltimateAbility")){
+            StartCoroutine("ActivateUltimate");
+            
         }
 
 
@@ -68,10 +71,24 @@ private Animator playerAnimation;
         if(Input.GetKey("left shift") && energy > 0 && movement.magnitude >0){
             Sprinting();
             energy--;
+            
         }
         else if (energy<100){
              energy = energy + 0.1f;
+             //playerRigidBody.isKinematic = true;
          }
+
+
+         if(Input.GetKey("e") && energy > 50){
+             playerBoxCollider.enabled = false;
+            playerRigidBody.AddForce(currentDirection*4000);
+            energy -= 50; 
+        }
+
+
+        playerBoxCollider.enabled = true;
+        
+       
        
 
         
@@ -93,10 +110,46 @@ private Animator playerAnimation;
     }
 
     void Sprinting(){   
-            playerRigidBody.AddForce(currentDirection*500);
+            playerRigidBody.AddForce(currentDirection*300);
     }
 
+    IEnumerator ActivateUltimate(){
+        playerAnimation.SetBool("ultimate", true);
+        for(float i  = 0.1f; i <1; i +=0.2f){
+            //playerAnimation.Play("ultimateAbility");
+            //playerAnimation.StopPlayback();
+            for(float j  = 0.1f; j <1; j +=0.2f){
+                yield return new WaitForSeconds(0.05f);
+                myAbility.Ability(transform.position, new Vector2(i, j), playerAnimation, playerRigidBody );
+                myAbility.Ability(transform.position, new Vector2(-i, j), playerAnimation, playerRigidBody );
+                myAbility.Ability(transform.position, new Vector2(i, -j), playerAnimation, playerRigidBody );
+                myAbility.Ability(transform.position, new Vector2(-i, -j), playerAnimation, playerRigidBody );
+            }
+            //playerAnimation.StopPlayback();
+            playerAnimation.SetBool("ultimate", false);
+                
 
+                
+        
+
+
+        // myAbility.Ability(transform.position, new Vector2(1f, 0f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(0.5f, 0f), playerAnimation, playerRigidBody );
+
+
+        // myAbility.Ability(transform.position, new Vector2(0f, 1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(1f, 0.5f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(1f, 1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(0.5f, 1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(-1f, 0f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(-0.5f, 1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(0f, -1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(1f, -0.5f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(-1f, -1f), playerAnimation, playerRigidBody );
+        // myAbility.Ability(transform.position, new Vector2(-1f, -0.5f), playerAnimation, playerRigidBody );
+        //yield return new WaitForSeconds(0.05f);
+        }
+    }
     
 
 }
