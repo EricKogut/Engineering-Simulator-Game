cc215909d7df16b26573821ef948675361d719e7
Juan Rodriguez <jrodri89@uwo.ca>
2020-03-17 19:53:13 -0400

    adding script to track player xp

---
+++ Eng Simulator/Assets/Scenes/_Scripts/Abilities/PlayerXP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerXP : MonoBehaviour
+{
+    private int experience;
+
+    public int Experience
+    {
+        get
+        {
+            return experience;
+        }
+        set
+        {
+            experience = value;
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return experience / 1000;
+        }
+        set
+        {
+            experience = value * 1000;
+        }
+    }
+}
