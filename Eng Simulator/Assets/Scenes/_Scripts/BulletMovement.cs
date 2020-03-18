d124e429c797fdcc5bc05161b5b58c3f75878db2
Juan Rodriguez <jrodri89@uwo.ca>
2020-03-17 21:02:50 -0400

    finished main customization panel

--- Eng Simulator/Assets/Scenes/_Scripts/BulletMovement.cs
+++ Eng Simulator/Assets/Scenes/_Scripts/BulletMovement.cs
@@ -6,41 +6,46 @@ using UnityEngine;
 //This will be used to move the bullet
 public class BulletMovement : MonoBehaviour
 {
-    
+
 
     //This rigidBody will be what the builder
     public Rigidbody2D theRigidBody;
-    
+
     //The speed will control 
     public float speed;
-    
+
     // Start is called before the first frame update
     void Start()
     {
 
         //Getting the RB component
-    theRigidBody = GetComponent<Rigidbody2D>();  
+        theRigidBody = GetComponent<Rigidbody2D>();
     }
 
 
     //This will actually move the bullet
-    public void movement(Vector2 direction){
-        theRigidBody.velocity = direction.normalized*speed;
+    public void movement(Vector2 direction)
+    {
+        theRigidBody.velocity = direction.normalized * speed;
         Debug.Log("MOVINGS");
         Debug.Log(direction);
     }
-    
-     void OnTriggerEnter2D(Collider2D theObject){
 
-        if(theObject.gameObject.CompareTag("enemy")){
+    void OnTriggerEnter2D(Collider2D theObject)
+    {
+
+        if (theObject.gameObject.CompareTag("enemy"))
+        {
             Destroy(theObject.gameObject);
             Destroy(this.gameObject);
+            // TODO: add player XP each time an enemy is killed
         }
-        if(theObject.gameObject.CompareTag("wall")){
+        if (theObject.gameObject.CompareTag("wall"))
+        {
             Destroy(this.gameObject);
         }
 
 
 
-     }
+    }
 }
