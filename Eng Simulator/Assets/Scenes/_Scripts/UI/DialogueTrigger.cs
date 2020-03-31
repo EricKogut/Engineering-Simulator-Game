e7f03819f59a98ef731732e5d228f0fa9793f29e
Juan Rodriguez <jrodri89@uwo.ca>
2020-03-30 20:28:41 -0400

    dialogue system and story outline

---
+++ Eng Simulator/Assets/Scenes/_Scripts/UI/DialogueTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    private GameObject[] _interlocutors;
+    private bool _triggered = false; // prevents any triggers after the first
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        _triggered = true;
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    public bool EnterRadius()
+    {
+        _interlocutors = GameObject.FindGameObjectsWithTag("player");
+
+        return (this.transform.position - _interlocutors[0].transform.position).magnitude < 3;
+        // if the distance between the dialogue NPC and the player is < 3, then trigger the dialogue
+
+    }
+
+    void Update()
+    {
+        if (EnterRadius() && !_triggered) TriggerDialogue();
+    }
+}
