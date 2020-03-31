351912b123717ba08dfb0d56a0ea3a0278d13603
gsimon321 <60612435+gsimon321@users.noreply.github.com>
2020-03-31 14:36:36 -0400

    quickStage

--- Eng Simulator/Assets/Scenes/_Scripts/FightingScripts/EnemyHealthManager.cs
+++ Eng Simulator/Assets/Scenes/_Scripts/FightingScripts/EnemyHealthManager.cs
@@ -34,4 +34,6 @@ public class EnemyHealthManager : MonoBehaviour
         enemyMaxHealth = max;
     }
 
+  
+
 }
---
+++ Eng Simulator/Assets/Scenes/_Scripts/FightingScripts/enemyShooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShooting : MonoBehaviour
+{
+
+    private Transform currentPosition;
+    public ShootingAbility primaryAbility;
+    new Vector3 currentDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = GameObject.FindGameObjectWithTag("player").transform;
+        currentDirection = currentPosition.position-transform.position;
+    Debug.Log(currentDirection);
+        //Starting the couroutine
+            StartCoroutine("Shooting");
+    }
+
+
+
+  //Ultimate Ability IEnumeration method
+    IEnumerator Shooting()
+    {
+        yield return new WaitForSeconds(0.05f);
+        primaryAbility.Ability(transform.position + currentDirection.normalized, currentDirection, null, null);
+   
+    }
+
+
+
+}
